c2cf774bff8ae82ea4058f7e6461e049d6bbf309
Misael <qmisael1023@gmail.com>
2021-10-04 20:53:14 -0700

    Modulos de Python

---
+++ main.py
@@ -0,0 +1,12 @@
+#import operations
+#print(operations.rest(10,5))
+from operations import sum,rest,mensaje
+
+print (sum(10,5))
+print (rest(10,5))
+print (mensaje)
+
+#sets, list, ciclo for y while
+
+
+
