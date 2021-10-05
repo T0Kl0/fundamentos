f0eedf5d9e9b3983f8192ecde41a4bc5a3d82522
Misael <qmisael1023@gmail.com>
2021-10-04 20:23:57 -0700

    Cadenas de Python

---
+++ origin.py
@@ -0,0 +1,19 @@
+name = "josue misael quintero luevanos"
+age = 10
+istall=True
+height = 1.80
+
+print ("{} tiene {} años".format(name,age))
+
+print (name+ " tiene " +str(age)+ " años")
+
+print(f"{name} tiene {age+10} años y es alto {istall} y mide {height}")
+
+print (name.upper())
+print (name.lower())
+print (name.title())
+
+print(name*5)
+
+
+
